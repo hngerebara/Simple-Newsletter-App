d4462d6b98d4a861c668de78a7934bc5c127028c
Hope Ngerebara <h.ngerebara@gmail.com>
2022-07-30 09:57:21 +0100

    add bootstrap form

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(express.static("public"))
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + "/index.html");
