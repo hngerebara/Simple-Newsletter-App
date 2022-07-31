f594332f5380a716c23e617b50b6f63f0f0e07b7
Hope Ngerebara <h.ngerebara@gmail.com>
2022-07-31 07:25:47 +0100

    post to mailchimp

--- app.js
+++ app.js
@@ -1,11 +1,54 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const https = require('https');
 
 const app = express();
-app.use(express.static("public"))
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+const url = 'https://us10.api.mailchimp.com/3.0/lists/808a684f7a';
 
 app.get('/', (req, res) => {
-	res.sendFile(__dirname + "/index.html");
+	res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (req, res) => {
+	const firstName = req.body.fname;
+	const lastName = req.body.lname;
+	const email = req.body.email;
+
+	const data = {
+		members: [
+			{
+				merge_fields: {
+					FNAME: firstName,
+					LNAME: lastName,
+				},
+				email_address: email,
+				status: 'subscribed',
+			},
+		],
+	};
+
+	const jsonData = JSON.stringify(data);
+
+	const options = {
+		method: 'POST',
+		auth: 'hopeaz1:311596de746f1ca02f5a12bfb7bf37b2-us10',
+	};
+
+	const request = https.request(url, options, (res) => {
+		res.on('data', (data) => {
+			console.log(JSON.parse(data));
+		});
+	});
+	request.write(jsonData);
+
+	request.on('error', (e) => {
+		console.error(e);
+	});
+	request.end();
 });
 
 app.listen(3000, () => {
